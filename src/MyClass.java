35fc8ab791aabde15053c56e6fcb22b1b68ae8ca
bulutmridvan <bulutmridvan@gmail.com>
2026-02-10 11:51:45 +0300

    v2

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,6 @@ public class MyClass {
     public String string1="Bu bir string'dir.";
     //Version v2
     public String string2="Bu da başka bir string'dir.";
-
+    //Version v3
+    public String string3="Bu çok daha başka bir string'dir.";
 }
