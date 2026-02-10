68bc9ed5cf8749c668ca2a91086ba9f8783d2359
bulutmridvan <bulutmridvan@gmail.com>
2026-02-10 12:18:30 +0300

    t1

--- src/MyClass.java
+++ src/MyClass.java
@@ -5,4 +5,7 @@ public class MyClass {
     public String string2="Bu da başka bir string'dir.";
     //Version v3
     public String string3="Bu çok daha başka bir string'dir.";
+    //Techpro branc ,version t1
+    public String string4="Bu techpro branch'inde oluşturulmuş bir string'dir.";
+
 }
