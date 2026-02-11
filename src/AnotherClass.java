d4fffa3ebda91f6b639832ab2c9d01278018ffe9
bulutmridvan <bulutmridvan@gmail.com>
2026-02-11 22:28:41 +0300

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -2,4 +2,6 @@ public class AnotherClass {
     //Techpro branch ,version t7
     public String string1="Biri sen,biri ben iki damla yaş aktı.";
 
+    //Techpro branch ,t8
+    public String string2="Parıldayıp duran insanlara bak.";
 }
